c294cb7db9e711ebe00276b80e53d0ebc965241c
nkr <nkgithubnarkuziev108@gmail.com>
2022-10-01 04:36:36 +0500

    added bubble.js

--- sorting-algorithms/bubble.js
+++ sorting-algorithms/bubble.js
@@ -1,5 +1,19 @@
-const base = [8, 5, 2, 9, 7, 4, 1, 3, 6, 0];
+const bubble = (base) => {
+	for (i = 0; i < base.length; i++) {
+		for (x = 0; x < base.length; x++) {
+			
+			let cur = base[i];
+			let next = base[x + 1];
 
-const bubble = () => {
-	
+			if (base[i] > base[x + 1]) {
+				base[i] = next;
+				base[x + 1] = cur;
+			} else { continue; }
+		}
+	}
+	base.reverse();
+
+	console.log(base);
 }
+
+bubble([8, 14, 5, 2, 9, 11, 7, 13, 4, 1, 3, 12, 15, 6, 0, 10]);
